4f45bad0437fd4666e63e6a0e5136d807529b8d8
kyun2022 <fm13071303@rx.tnc.ne.jp>
2025-04-04 10:09:14 +0900

    🐛 軽微な修正

--- src/pages/Timer.tsx
+++ src/pages/Timer.tsx
@@ -26,7 +26,7 @@ const Page: NextPage = () => {
           </div>
 
           <div className="mt-4 text-center">
-            <div className="font-mono text-center text-6xl tracking-wider text-gray-800">
+            <div className="text-center text-6xl tracking-wider text-gray-800">
               {formattedTime}
             </div>
 
